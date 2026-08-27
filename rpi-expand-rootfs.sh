9005a2d2f47f028edae053c7641ef5010d81bb64
Ben Fiola <benfiola@gmail.com>
2026-08-26 22:57:27 -0700

    fix root device string processing

--- rpi-expand-rootfs.sh
+++ rpi-expand-rootfs.sh
@@ -4,7 +4,7 @@ set -e
 echo "Starting root filesystem expansion..."
 
 ROOT_PARTITION="$(realpath "$(findmnt -n -o SOURCE /)")"
-ROOT_DEVICE=$(echo "$ROOT_PARTITION" | sed 's/p[0-9]\+$//' | sed 's/[0-9]\+$//')
+ROOT_DEVICE=$(echo "$ROOT_PARTITION" | sed -E 's/p?[0-9]+$//')
 PARTITION_NUM=$(echo "$ROOT_PARTITION" | grep -o '[0-9]\+$')
 
 if [ -z "$ROOT_PARTITION" ] || [ -z "$PARTITION_NUM" ] || [ -z "$ROOT_DEVICE" ]; then
